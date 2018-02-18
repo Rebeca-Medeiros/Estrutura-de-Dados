a90f5f70c8d60e5ad81f9e24f2f98b6b967de5de
Rebeca Medeiros <rebeca.andrade.medeiros@gmail.com>
2018-02-18 20:29:48 -0300

    Create complexo1.c

---
+++ Exercicio 1/complexo1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "complex1.h"
+
+tComplexo CriaComplexo(double x, double y) {
+	tComplexo c;
+	c.pReal = x;
+	c.pImaginaria = y;
+	return c;
+}
+
+tComplexo SomaComplexos(tComplexo c1, tComplexo c2) {
+	tComplexo c;
+	c.pReal = c1.pReal + c2.pReal;
+	c.pImaginaria = c1.pImaginaria + c2.pImaginaria;
+	return c;
+}
+
+void ExibeComplexo(tComplexo c){
+	printf("%2.1lf + %2.1lfi", c.pReal, c.pImaginaria);
+}
