186576535fc3c062ed386f3a6b187f7d90b9b63d
Rebeca Medeiros <rebeca.andrade.medeiros@gmail.com>
2018-04-24 11:53:04 -0300

    Add files via upload

--- Recursividade/Fatorial.c
+++ Recursividade/Fatorial.c
@@ -1,1 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
 
+int Fatorial(int n){
+   if(n <= 1){
+        return 1;
+    } else {
+        return n * Fatorial(n - 1);
+    }
+}
+
+int main(){
+    int i;
+
+    printf("Digite um numero: ");
+    scanf("%d", &i);
+
+    Fatorial(i);
+
+    return 0;
+}
