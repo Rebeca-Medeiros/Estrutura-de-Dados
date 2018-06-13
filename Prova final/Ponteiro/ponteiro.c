884e01f52c71f075c0ceb94927846355a4476738
Rebeca Medeiros <rebeca.andrade.medeiros@gmail.com>
2018-06-13 14:29:21 -0300

    Add files via upload

--- Prova final/Ponteiro/ponteiro.c
+++ Prova final/Ponteiro/ponteiro.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(){
+    int meuInteiro;
+    int *ponteiroParaInteiro; // Vai apontar para a variavel int
+
+    ponteiroParaInteiro = &meuInteiro; // Ponteiro iniciado com endereço da variavel
+
+    float meuFloat = 3.14;
+    float *ponteiroParaFloat = &meuFloat;
+
+    printf("%f", meuFloat);
+
+    puts("");
+
+    *ponteiroParaFloat = 1.6;
+
+    printf("%f", meuFloat);
+
+    puts("");
+
+    return 0;
+}
